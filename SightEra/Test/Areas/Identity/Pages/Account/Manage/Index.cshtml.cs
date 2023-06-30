1783182d4d525c97766295a290b83601ccfa9c65
Ahmed Saber <138237242+AhmedSaber07@users.noreply.github.com>
2023-06-30 21:15:33 +0300

    Add files via upload

---
+++ SightEra/Test/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Test.Data;
+using Test.Models;
+
+namespace Test.Areas.Identity.Pages.Account.Manage
+{
+    public partial class IndexModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public string Username { get; set; }
+        public string Usertype { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [RegularExpression("^[A-Za-z]+$", ErrorMessage = "Please Enter Valid Name")]
+            [Required]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+            [RegularExpression("^[A-Za-z]+$", ErrorMessage = "Please Enter Valid Name")]
+            [Required]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            [Display(Name = "Profile Picture")]
+            public byte[] ProfilePicture { get; set; }
+        }
+
+        private async Task LoadAsync(ApplicationUser user)
+        {
+            var userName = await _userManager.GetUserNameAsync(user);
+            var userType = user.UserType;
+                // var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+
+            Username = userName;
+            Usertype = userType;
+            Input = new InputModel
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                ProfilePicture = user.ProfilePicture
+            };
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            //var user_context;
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+            if (user.UserType == "Doctor")
+            {
+                var user_context = _context.Doctors.FirstOrDefault(e => e.UserName == user.UserName);
+            }
+            else
+            {
+                var user_context = _context.Radiologists.FirstOrDefault(e => e.UserName == user.UserName);
+            }
+            await LoadAsync(user);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await LoadAsync(user);
+                return Page();
+            }
+            var firstName = user.FirstName;
+            var lastName = user.LastName;
+
+            if(Input.FirstName!=firstName)
+            {
+                user.FirstName = Input.FirstName;
+                await _userManager.UpdateAsync(user);
+            }
+
+            if (Input.LastName != lastName)
+            {
+                user.LastName = Input.LastName;
+                await _userManager.UpdateAsync(user);
+            }
+            if (Request.Form.Files.Count > 0)
+            {
+                var file = Request.Form.Files.FirstOrDefault();
+                using (var dataStraem = new MemoryStream())
+                {
+                    await file.CopyToAsync(dataStraem);
+                    user.ProfilePicture = dataStraem.ToArray();
+                }
+                await _userManager.UpdateAsync(user);
+            }
+
+            if (user.UserType == "Doctor")
+            {
+                var user_context = _context.Doctors.FirstOrDefault(e => e.UserName == user.UserName);
+                if (Input.FirstName != user_context.FirstName)
+                {
+                    user_context.FirstName = Input.FirstName;
+                    EntityEntry entityEntry = _context.Entry(user_context);
+                    entityEntry.State = EntityState.Modified;
+                    _context.SaveChanges();
+                }
+
+                if (Input.LastName != user_context.LastName)
+                {
+                    user_context.LastName = Input.LastName;
+                    EntityEntry entityEntry = _context.Entry(user_context);
+                    entityEntry.State = EntityState.Modified;
+                    _context.SaveChanges();
+                }
+                if (Request.Form.Files.Count > 0)
+                {
+                    var file = Request.Form.Files.FirstOrDefault();
+                    using (var dataStraem = new MemoryStream())
+                    {
+                        await file.CopyToAsync(dataStraem);
+                        user_context.ProfilePicture = dataStraem.ToArray();
+                    }
+                    EntityEntry entityEntry = _context.Entry(user_context);
+                    entityEntry.State = EntityState.Modified;
+                    _context.SaveChanges();
+                }
+            }
+            else
+            {
+                var user_context = _context.Radiologists.FirstOrDefault(e => e.UserName == user.UserName);
+                if (Input.FirstName != user_context.FirstName)
+                {
+                    user_context.FirstName = Input.FirstName;
+                    EntityEntry entityEntry = _context.Entry(user_context);
+                    entityEntry.State = EntityState.Modified;
+                    _context.SaveChanges();
+                }
+
+                if (Input.LastName != user_context.LastName)
+                {
+                    user_context.LastName = Input.LastName;
+                    EntityEntry entityEntry = _context.Entry(user_context);
+                    entityEntry.State = EntityState.Modified;
+                    _context.SaveChanges();
+                }
+                if (Request.Form.Files.Count > 0)
+                {
+                    var file = Request.Form.Files.FirstOrDefault();
+                    using (var dataStraem = new MemoryStream())
+                    {
+                        await file.CopyToAsync(dataStraem);
+                        user_context.ProfilePicture = dataStraem.ToArray();
+                    }
+                    EntityEntry entityEntry = _context.Entry(user_context);
+                    entityEntry.State = EntityState.Modified;
+                    _context.SaveChanges();
+                }
+            }
+            //var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            //if (Input.PhoneNumber != phoneNumber)
+            //{
+            //    var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+            //    if (!setPhoneResult.Succeeded)
+            //    {
+            //        StatusMessage = "Unexpected error when trying to set phone number.";
+            //        return RedirectToPage();
+            //    }
+            //}
+
+            await _signInManager.RefreshSignInAsync(user);
+            StatusMessage = "Your profile has been updated";
+            return RedirectToPage();
+        }
+    }
+}
---
+++ SightEra/Test/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+using Test.Data;
+using Test.Models;
+
+namespace Test.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
+        private readonly ApplicationDbContext _context;
+
+        public RegisterModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ILogger<RegisterModel> logger,
+            IEmailSender emailSender,
+            ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
+            _context = context;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [RegularExpression("^[A-Za-z]+$", ErrorMessage = "Please Enter Valid Name")]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [RegularExpression("^[A-Za-z]+$", ErrorMessage = "Please Enter Valid Name")]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            [Required]
+            public string UserType { get; set; }
+            
+            [Display(Name = "Profile Picture")]
+            public byte[] ProfilePicture { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$", ErrorMessage = "Please Enter Strong Password")]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 8)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+        }
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser 
+                {
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    UserName = new MailAddress(Input.Email).User,
+                    Email = Input.Email,
+                    UserType = Input.UserType
+                };
+                if(Request.Form.Files.Count>0)
+                {
+                    var file = Request.Form.Files.FirstOrDefault();
+                    using(var dataStraem = new MemoryStream())
+                    {
+                        await file.CopyToAsync(dataStraem);
+                        user.ProfilePicture = dataStraem.ToArray();
+                    }
+                }
+                var result = await _userManager.CreateAsync(user, Input.Password);
+                await _userManager.AddToRoleAsync(user, Input.UserType);
+                if (Input.UserType == "Doctor")
+                {
+                    var doctor = new Doctor()
+                    {
+                        FirstName = user.FirstName,
+                        LastName = user.LastName,
+                        Email = user.Email,
+                        UserName = user.UserName,
+                        ProfilePicture = user.ProfilePicture
+                    };
+                    _context.Doctors.Add(doctor);
+                    _context.SaveChanges();
+                }
+                else if (Input.UserType == "Radiologist")
+                {
+                    var radiologist = new Radiologist()
+                    {
+                        FirstName = user.FirstName,
+                        LastName = user.LastName,
+                        Email = user.Email,
+                        UserName = user.UserName,
+                        ProfilePicture = user.ProfilePicture
+                    };
+                    _context.Radiologists.Add(radiologist);
+                    _context.SaveChanges();
+                }
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+
+                    /* if (Input.UserType == "Doctor")
+                     {
+                         await _userManager.AddToRoleAsync(user, "Doctor");
+                     }
+                     else
+                     {
+                         await _userManager.AddToRoleAsync(user, "Radiologist");
+                     }*/
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                        protocol: Request.Scheme);
+
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                    }
+                    else
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        return LocalRedirect(returnUrl);
+                    }
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+    }
+}
